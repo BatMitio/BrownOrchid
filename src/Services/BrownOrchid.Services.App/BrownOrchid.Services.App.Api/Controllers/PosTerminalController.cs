6da48fed0efa4637fbecaa226100d6ed87b9f922
BatMitio <mitko_zlia@abv.bg>
2022-06-19 06:09:03 +0300

    posterminal web api done

---
+++ src/Services/BrownOrchid.Services.App/BrownOrchid.Services.App.Api/Controllers/PosTerminalController.cs
@@ -0,0 +1,28 @@
+﻿using BrownOrchid.Services.App.Queries.QueryDiscounts;
+using BrownOrchid.Services.App.Queries.QueryPosTerminals;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrownOrchid.Services.App.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class PosTerminalController : ControllerBase
+{
+    private IMediator _mediator;
+
+    public PosTerminalController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Authorize(AuthenticationSchemes = "Bearer", Policy = "Employee")]
+    public async Task<IActionResult> All()
+    {
+        var query = new QueryPosTerminals();
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+}
